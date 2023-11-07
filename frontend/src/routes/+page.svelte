ef8f665bc5d2a8580595a4183931de63566a1e7a
Leonard Ruebel <leonard.ruebel@gmail.com>
2023-11-07 22:49:56 +0100

    startpage + slug zeug

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1 class="bg-slate-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let groupID = '';
+</script>
+
+<div class="hero min-h-screen">
+	<div class="hero-content flex-col">
+		<div class="card max-w-3xl w-full bg-neutral">
+			<div class="card-body gap-4">
+				<div class="card-title">Group ID</div>
+				<input type="text" class="input" bind:value={groupID} />
+				<div class="card-actions justify-center">
+					<a href="/{groupID}" class="btn btn-primary">Join</a>
+				</div>
+			</div>
+		</div>
+		<div class="card max-w-sm w-full bg-neutral">
+			<form class="card-body">
+				<div class="form-control">
+					<button class="btn btn-primary">Create Group</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
